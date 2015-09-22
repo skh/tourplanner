715928cf6a953b48b1fce8b95f047d7d352a6db5
Sonja Krause-Harder <skh@rcane.de>
2015-09-22 21:00:30 +0200

    Initial javascript skeleton

---
+++ js/tourplanner.js
@@ -0,0 +1,10 @@
+// a place is anything that is returned by Yelp 
+// and will be displayed on the map
+var Place = function () {};
+
+// Main data will be directly kept in the ViewModel object
+var ViewModel = function () {
+
+}
+
+ko.applyBindings (new ViewModel());
