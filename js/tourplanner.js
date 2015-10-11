d4b03c760ab9a89312a7026db516cec7e9f6daeb
Sonja Krause-Harder <skh@rcane.de>
2015-10-11 19:44:43 +0200

    Move load of google map javascript to Map

--- js/tourplanner.js
+++ js/tourplanner.js
@@ -5,10 +5,15 @@ var Place = function () {};
 var Map = function () {
 	this.init = function () {
 		this.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-	}
+			center: {lat: -34.397, lng: 150.644},
+			zoom: 8
+		});
+	};
+	$.getScript("https://maps.googleapis.com/maps/api/js?key="
+		+ config.maps_api_key, (function () {
+			console.log(this);
+			this.init();
+		}).bind(this));
 };
 
 // Main data will be directly kept in the ViewModel object
@@ -18,15 +23,11 @@ var ViewModel = function () {
 		// if not, set up with hard-coded data
 		this.city = ko.observable("Nuremberg");
 		this.map = new Map();
-		$.getScript("https://maps.googleapis.com/maps/api/js?key="
-		+ config.maps_api_key + "&callback=vm.map.init", (function () {
-			console.log(this);
-		}).bind(this));
+
 
 	};
 
 	this.init();
 }
 
-var vm = new ViewModel();
-ko.applyBindings (vm);
+ko.applyBindings (new ViewModel());
