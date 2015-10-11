6f55f4d27e78c1b60be86fb0ba8f7e2466098a62
Sonja Krause-Harder <skh@rcane.de>
2015-10-11 17:54:15 +0200

    Add Maps API & map

--- js/tourplanner.js
+++ js/tourplanner.js
@@ -14,3 +14,17 @@ var ViewModel = function () {
 }
 
 ko.applyBindings (new ViewModel());
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+var maps_api_url = "https://maps.googleapis.com/maps/api/js?key="
+	+ config.maps_api_key + "&callback=initMap";
+
+$.getScript(maps_api_url);
+initMap();
