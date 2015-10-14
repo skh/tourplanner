a47cb55dd92b866d92eef530503c80144bf2acbe
Sonja Krause-Harder <skh@rcane.de>
2015-10-14 22:48:21 +0200

    Add geocoder to search for city.

--- js/tourplanner.js
+++ js/tourplanner.js
@@ -4,10 +4,23 @@ var Place = function () {};
 
 var Map = function () {
 	this.init = function () {
+		this.geocoder = new google.maps.Geocoder();
 		this.map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: -34.397, lng: 150.644},
 			zoom: 8
 		});
+		this.geocoder.geocode( { 'address': 'Nuernberg'}, (function(results, status) {
+			console.log (this);
+			if (status == google.maps.GeocoderStatus.OK) {
+				this.map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: this.map,
+					position: results[0].geometry.location
+				});
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		}).bind(this));
 	};
 	$.getScript("https://maps.googleapis.com/maps/api/js?key="
 		+ config.maps_api_key + "&callback=map.init");
