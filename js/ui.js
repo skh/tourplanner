ec91ec98518e2e2bd40acc239f67216099698ffa
Sonja Krause-Harder <skh@rcane.de>
2015-10-31 14:24:41 +0100

    Make panel draggable, map full-screen.

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+// this file contains UI-specific code that is not part of the actual application
+
+
+// allow the user to drag the panel out of the way
+$(function() {
+	$("#panel").draggable();
+});
+
